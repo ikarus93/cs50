33441f2eae9fca01c945603707b78f8573685dca
Farid Wilhelm Zimmermann <ikarus9939@gmail.com>
2018-03-25 16:30:51 +0000

    finished vigenere

---
+++ pset2/crack/crack.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+#define _XOPEN_SOURCE
+#include <unistd.h>
+
+int main(int argc, string argv[])
+{
+    //If commandline arguments are not sufficient
+    if (argc != 2)
+    {
+        printf("Usage: ./vigenere k\n");
+        return 1;
+    }
+
+}
