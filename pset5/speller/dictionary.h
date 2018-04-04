0104b1917b555582ea167e1d249c9a76379cdd40
Farid Wilhelm Zimmermann <ikarus9939@gmail.com>
2018-04-04 17:41:02 +0000

    Finished PSET 5

--- pset5/speller/dictionary.h
+++ pset5/speller/dictionary.h
@@ -9,6 +9,13 @@
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
 
+//node struct
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
 // Prototypes
 bool check(const char *word);
 bool load(const char *dictionary);
